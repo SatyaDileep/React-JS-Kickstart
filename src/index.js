408ef4ecb472355d02cbfae7bf1ac5574ba862e6
SatyaDileep <SatyaDileepKumar.Thotakura@in.pega.com>
2020-01-26 13:46:34 +0530

    adding json file for practice

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const myfirstelement = <h1>Hello React!</h1>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const tableEle = (<div>
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+  </div>
+);
+ReactDOM.render(tableEle, document.getElementById('root'));
